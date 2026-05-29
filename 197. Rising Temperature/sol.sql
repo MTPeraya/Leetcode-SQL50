d7509a90e70094aa3bc0e389f515d57872cd82a1
MTPeraya <m.perayalsc@gmail.com>
2026-05-29 21:39:21 +0700

    change sol 197 for faster runtime

--- 197. Rising Temperature/sol.sql
+++ 197. Rising Temperature/sol.sql
@@ -1,5 +1,4 @@
-SELECT w1.id
-FROM Weather w1
-JOIN Weather w2 
-  ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
-WHERE w1.temperature > w2.temperature;
+SELECT w2.id AS id
+FROM Weather w2
+JOIN Weather w1 ON w1.recordDate = DATE_SUB(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature < w2.temperature;
